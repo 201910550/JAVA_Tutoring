fe2d3c413b072135875b1cb22e478ca57bf8127a
201910550 <79843824+201910550@users.noreply.github.com>
2021-05-28 23:10:37 +0900

    Delete tutoring_hm1_main.java

--- tutoring_hm1_main.java
+++
@@ -1,6 +0,0 @@
-public class tutoring_hm1_main {
-    public static void main(String[] args){
-        tutoring_hm1_ColorTV myTV = new tutoring_hm1_ColorTV(32, 1024);
-        myTV.printProperty();
-    }
-}
